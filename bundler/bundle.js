0ef8f611c45660599d1fe868bdee05fa8de9aa51
zhorton <zak.horton@vml.com>
2020-05-07 20:42:46 -0500

    Updated bundle to use vue.md example of vuejs-form alongside vuejs-validators

--- bundler/bundle.js
+++ bundler/bundle.js
@@ -11,6 +11,8 @@ const api = readFileSync('bundler/api.md', 'utf-8');
 const extend = readFileSync('bundler/extend.md', 'utf-8');
 const hooks = readFileSync('bundler/hooks.md', 'utf-8');
 const errors = readFileSync('bundler/errors.md', 'utf-8');
+const vue = readFileSync('bundler/vue.md', 'utf-8');
+
 // const strictnessAndComparisons = readFileSync('bundler/strictness_and_comparisons.md', 'utf-8');
 // const notImplemented = readFileSync('bundler/not_implemented.md', 'utf-8');
 const contribute = readFileSync('bundler/contribute.md', 'utf-8');
@@ -46,8 +48,9 @@ writeFileSync(
 	'README.md',
 	[
 		badges,
-		header,
 		installation,
+		vue,
+		header,
 		api,
 		tableOfContents,
 		methodDocumentation,
